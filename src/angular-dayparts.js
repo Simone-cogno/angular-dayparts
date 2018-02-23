73befada3c1bb3162475bc323a04fb9b0de5ee26
Simone Cogno <scogno@snowflake.ch>
2018-02-23 17:08:59 +0100

    [FIX] Fix numbers days

--- src/angular-dayparts.js
+++ src/angular-dayparts.js
@@ -20,7 +20,7 @@ angular.module('angular-dayparts', [])
               $scope.days = westernWeek;
             }
             
-            $scope.hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+            $scope.hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
             var klass = 'selected';
             var startCell = null;
